c28a2e6e51e90e229e12e4c05a52940b294efcfe
dairof7 <dairof7@gmail.com>
2020-09-26 16:38:45 -0500

    adding task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     if (values.length === 0) $('.amenities h4').html('&nbsp;');
   })
   
-  const url = 'http://127.0.0.1:5001/api/v1/status/';
+  const url = 'http://127.0.0.1:5001/api/v1/places_search/';
   $.get(url, function (info) {
     if (info.status === 'OK'){
       $('#api_status').addClass('available');
@@ -21,4 +21,43 @@ $(document).ready(function () {
       $('#api_status').removeClass('available');
     }
   });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (places) {
+      places.forEach(place => {
+        const s = (place.max_guest !== 1) ? 's' : '';
+        const s2 = (place.number_rooms !== 1) ? 's' : '';
+        const s3 = (place.number_bathrooms !== 1) ? 's' : '';
+        const html = `<article>
+        <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">$${place.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest} Guest${s}</div>
+          <div class="number_rooms">${place.number_rooms}
+            Bedroom${s2}</div>
+          <div class="number_bathrooms">${place.number_bathrooms}
+            Bathroom${s3}</div>
+        </div>
+        <div class="user">
+          
+        </div>
+        <div class="description">
+          ${place.description}
+        </div>
+      </article>`;
+        $('.places').append(html);
+      });
+    }
+  });
+  /* */
+
+  /* End Show places dynamic mode */
+
 })
